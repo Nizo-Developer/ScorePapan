5cde8ef4440d423c8aed3d240dbe17d7dbb2b102
Rafa Hylmi Azzami <152425765+hylmiaz@users.noreply.github.com>
2023-11-30 13:20:15 +0700

    bug fix - ganti otak sementara

--- Proyek/otak.js
+++ Proyek/otak.js
@@ -1,24 +1,11 @@
 var score1 = 1;
 var score2 = 1;
 
-fetch("memo-otak.json")
-    .then(response => response.json())
-    .then(data => {
-        co = data
-    })
-    .catch(error => console.error('Error:', error));
-
-function cl(conlog) {
-    console.log(conlog)
-}
-
-function inputing(team) {
+function inputing(team, num) {
     //Membuat Baris (tr)
     var baris = document.createElement("tr");
     //Menyeting Atribut dari tr di atas (Atribut, Value)
     baris.setAttribute("id", "baris");
-
-    var noda = co.notif
 
     if (team == 0) {
         var p = 2
@@ -35,8 +22,7 @@ function inputing(team) {
         //Menambahkan si kolom (tr) ke baris (td)
         //Sistemnya kyk variabel yang belum di print
         baris.appendChild(kolom)
-        cl("i" + i + noda)
-        if (i == noda && k == 2) {
+        if (i == num && k == 2) {
             if (team == 1) {
                 kolom.innerHTML = score1;
                 score1++
@@ -44,9 +30,8 @@ function inputing(team) {
                 kolom.innerHTML = score2;
                 score2++
             };
-            cl("yes")
-        } else if (i == noda && k == 1) {
-            if (noda == 1) {
+        } else if (i == num && k == 1) {
+            if (num == 1) {
                 kolom.innerHTML = score1;
                 score1++
             } else {
@@ -56,56 +41,9 @@ function inputing(team) {
         } else {
             kolom.innerHTML = ' '
         }
-        cl(score1)
-        cl(score2)
     }
 
     var dc = document.getElementById("dataContent");
     //Kalau ini anggep aja print karna kita ambil data dari element dari id yang ada di tag html di atas scroll cari komentar "yang ini"
     dc.appendChild(baris)
-    co.notif = parseInt(0)
-    cl("aa" + co.notif)
-}
-
-function notif(con, p, team) {
-    var not = document.getElementById("notif");
-    var nobg = document.getElementById("notif-bg");
-    var sub = document.getElementsByClassName("sub");
-    co.notif = parseInt(p)
-    cl("ini" + co.notif)
-
-    if (co.notif > 0) {
-        for (var i = 0; i < sub.length; i++) {
-            sub[i].addEventListener("mouseup", tp);
-            cl(i)
-        }
-
-        function tp(event) {
-            var mouseX = event.clientX;
-            var mouseY = event.clientY - 150;
-
-            not.style.left = mouseX + 'px';
-            not.style.top = mouseY + 'px';
-            kondisi()
-        };
-    } else {
-        kondisi()
-    }
-    
-    function kondisi() {
-        if (con == 1) {
-            not.style.display = "flex";
-            nobg.style.display = "flex";
-        } else if (con == 2) {
-            inputing(team)
-            not.style.display = "none";
-            nobg.style.display = "none";
-        } else {
-            co.notif = parseInt(0)
-            not.style.display = "none";
-            nobg.style.display = "none";
-        }   
-    }
-
-    
 }
